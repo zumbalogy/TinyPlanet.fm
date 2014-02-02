24f8d714b21c0e708ea705f9f764bb1648fd9c8a
zumbalogy <intrapostal@gmail.com>
2014-02-02 11:15:07 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinyplanet::Application.config.secret_token = '1d1d2637e1f773144b29dd0dfb00d8b43f7644dc9ee32bb3973c31cb92a3b9fd72b4fa44e50127b9b3c3b2fcb5d7dad392c865629a4da164886970b98f517e4b'
