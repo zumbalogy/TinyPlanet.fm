170d3bbd983b73daf1625104e3a3af3b4f200ace
zumbalogy <intrapostal@gmail.com>
2014-02-02 19:18:10 -0500

    mostly cosmetic changes, none of which are permanate

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
     def test
         client = SoundCloud.new(:client_id => '4c2a3b5840e0236549608f59c2cd7d07')
 
-        tracks = client.get('/tracks', q: params[:q], genres: params[:genre] )
+        tracks = client.get('/tracks', q: params[:city], genres: params[:genre] )
         respond_to do |format|
             format.json { render json: tracks.to_json }
         end
