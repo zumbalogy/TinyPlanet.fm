9898d73206fd25794a0d65aa37bf542b7355abd0
zumbalogy <intrapostal@gmail.com>
2014-02-03 18:00:41 -0500

    saving songs and combos seems to work

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,24 @@ class MainController < ApplicationController
         client = SoundCloud.new(:client_id => '4c2a3b5840e0236549608f59c2cd7d07')
 
         tracks = client.get('/tracks', q: params[:city], genres: params[:genre] )
+
+        combo = Combo.new
+        combo.city = params[:city]
+        combo.genre = params[:genre]
+        combo.save
+
+        tracks.each do |track|
+            foo = Song.new
+            foo.combo_id = combo.id
+            foo.name = track.title
+            foo.url = track.uri
+            foo.soundcloud_id = track.id
+            foo.waveform_url = track.waveform_url
+            foo.artwork_url =
+            foo.artist = 
+            foo.save
+        end
+
         respond_to do |format|
             format.json { render json: tracks.to_json }
         end
@@ -18,7 +36,7 @@ class MainController < ApplicationController
         foo = Opinion.new
         foo.song_id = Song.find(params[:song_id])
         foo.user_id = User.find(params[:user_id])
-        foo.enjoyed? = params[:opinion]
+        foo.enjoyed = params[:opinion]
         foo.save
     end
 
