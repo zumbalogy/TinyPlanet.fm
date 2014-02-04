d3a978f30f11a0196cab2c31eeb1efca5a0de5f6
Matthew Plummer <plummat@gmail.com>
2014-02-04 14:40:48 -0500

    fixed font glitch

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ end
 
 module Tinyplanet
   class Application < Rails::Application
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    # config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
