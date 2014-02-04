be22b30b8950aa33266899c28a48c3c0c12040f4
zumbalogy <intrapostal@gmail.com>
2014-02-04 14:20:06 -0500

    check before save combo

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,27 +1,33 @@
 class MainController < ApplicationController
 
     def save
+        check = true
         genre_check = Combo.find_by_genre(params[:genre])
-        
-        
-        @client = SoundCloud.new(:client_id => '4c2a3b5840e0236549608f59c2cd7d07')
-        @tracks = @client.get('/tracks', q: params[:city], genres: params[:genre])
- 
-        @combo = Combo.new
-        @combo.city = params[:city]
-        @combo.genre = params[:genre]
-        @combo.save
-
-        @tracks.each do |track|
-            song = Song.new
-            song.combo_id = @combo.id
-            song.name = track.title
-            song.url = track.uri
-            song.soundcloud_id = track.id
-            song.waveform_url = track.waveform_url
-            song.artwork_url = track.artwork_url
-            song.artist = track.user.username
-            song.save
+        genre_check.each do |combo|
+            if combo.city == params[:city]
+                check = false
+            end
+        end
+        if check
+            @client = SoundCloud.new(:client_id => '4c2a3b5840e0236549608f59c2cd7d07')
+            @tracks = @client.get('/tracks', q: params[:city], genres: params[:genre])
+     
+            @combo = Combo.new
+            @combo.city = params[:city]
+            @combo.genre = params[:genre]
+            @combo.save
+    
+            @tracks.each do |track|
+                song = Song.new
+                song.combo_id = @combo.id
+                song.name = track.title
+                song.url = track.uri
+                song.soundcloud_id = track.id
+                song.waveform_url = track.waveform_url
+                song.artwork_url = track.artwork_url
+                song.artist = track.user.username
+                song.save
+            end
         end
     end
     
