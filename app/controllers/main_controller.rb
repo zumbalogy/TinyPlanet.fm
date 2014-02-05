03b40575b5a5f97f2eabf7abf4a1aef878bf809e
Zack D <zackdhome@gmail.com>
2014-02-05 13:43:37 -0800

    cleanup

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
 
-    def save
+  def save
     check = Combo.where("city = ? AND genre = ?", params[:city],params[:genre])
     if check.length == 0
       @client = SoundCloud.new(:client_id => '4c2a3b5840e0236549608f59c2cd7d07')
@@ -24,66 +24,68 @@ class MainController < ApplicationController
     else
       puts "already created!"
     end
-    # head :created, location: @client
-    render :nothing => true
+  # head :created, location: @client
+  render :nothing => true
   end
     
-    def serve
-        combo = Combo.where("city = ? AND genre = ?", params[:city],params[:genre])
-        unliked = []
-        liked = []
-        high_count = 0
-        combo.songs.each do |song|
-            if song.users.count == 0
-                unliked.push(song)
-            else
-                if song.users.count > high_count
-                    high_count = song.users.count
-                    liked << liked_most if liked_most
-                    liked_most = song
-                else
-                    liked.push(song)
-                end
-            end
-        end
-        liked.shuffle!
-        unliked.shuffle!
-        rand_front = unliked.shift(liked.length * 2)
-        rand_front += liked
-        rand_front.shuffle!
-        rand_front.unshift(liked_most)
-        @playlist = rand_front + unliked
-        
-        respond_to do |format|
-            format.json  { render :json => @playlist.to_json }
-        end
+  def serve
+    combo = Combo.where("city = ? AND genre = ?", params[:city],params[:genre])
+    unliked = []
+    liked = []
+    high_count = 0
+
+    combo.songs.each do |song|
+      if song.users.count == 0
+          unliked.push(song)
+      else
+          if song.users.count > high_count
+              high_count = song.users.count
+              liked << liked_most if liked_most
+              liked_most = song
+          else
+              liked.push(song)
+          end
+      end
+    end
+
+    liked.shuffle!
+    unliked.shuffle!
+    rand_front = unliked.shift(liked.length * 2)
+    rand_front += liked
+    rand_front.shuffle!
+    rand_front.unshift(liked_most)
+    @playlist = rand_front + unliked
     
-
-    def index
+    respond_to do |format|
+        format.json  { render :json => @playlist.to_json }
     end
+  end
+    
+  def index
+  end
 
-    def opinion
-        find = Opinion.where("song_id = ? AND user_id = ?", params[:song_id], current_user)
-        if find == []
-            foo = Opinion.new
-            foo.song_id = Song.find(params[:song_id])
-            foo.user_id = User.find(current_user) 
-            foo.enjoyed = true
-            foo.save
-        else
-            find.delete
-        end
+  def opinion
+    find = Opinion.where("song_id = ? AND user_id = ?", params[:song_id], current_user)
+    if find == []
+        foo = Opinion.new
+        foo.song_id = Song.find(params[:song_id])
+        foo.user_id = User.find(current_user) 
+        foo.enjoyed = true
+        foo.save
+    else
+        find.delete
     end
+  end
 
-    def favorite
-        find = Favorite.where("combo_id = ? AND user_id = ?", params[:combo_id], current_user))
-        if find == []
-            foo = Favorite.new
-            foo.user_id = current_user
-            foo.combo_id = params[:combo_id]
-            foo.save
-        else
-            find[0].destroy
-        end
+  def favorite
+    find = Favorite.where("combo_id = ? AND user_id = ?", params[:combo_id], current_user))
+    if find == []
+        foo = Favorite.new
+        foo.user_id = current_user
+        foo.combo_id = params[:combo_id]
+        foo.save
+    else
+        find[0].destroy
     end
+  end
 end
